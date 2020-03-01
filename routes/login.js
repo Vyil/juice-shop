2375e00ecdf71cd71efbb320da91b916aabd92d6
Djim <oomesdjim@gmail.com>
2020-03-01 12:38:17 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -35,19 +35,19 @@ PreparedStatement pstmt = connection.prepareStatement( query );
 pstmt.setString( 1, custname); 
 ResultSet results = pstmt.executeQuery( );
     */
-   var email = req.body.email;
-   var password = req.body.password;
-   let preparedStatement1= new sql.PreparedStatement(),
-    query = `SELECT * FROM Users WHERE (email = @email AND password = @password) AND deletedAt IS NULL`;
+  //  var email = req.body.email;
+  //  var password = req.body.password;
+  //  let preparedStatement1= new sql.PreparedStatement(),
+  //   query = `SELECT * FROM Users WHERE (email = @email AND password = @password) AND deletedAt IS NULL`;
 
-    preparedStatement1.input('email',sqlVarChar(50))
-    preparedStatement1.input('password',sqlVarChar(50))
-    preparedStatement1.prepare(query)
-    .then(function(){
-      return preparedStatement1.execute({email:email, password:password})
-    })   
-    // models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${insecurity.hash(req.body.password || '')}
-    // ' AND deletedAt IS NULL`, { model: models.User, plain: true })
+  //   preparedStatement1.input('email',sqlVarChar(50))
+  //   preparedStatement1.input('password',sqlVarChar(50))
+  //   preparedStatement1.prepare(query)
+  //   .then(function(){
+  //     return preparedStatement1.execute({email:email, password:password})
+  //   })   
+    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${insecurity.hash(req.body.password || '')}
+    ' AND deletedAt IS NULL`, { model: models.User, plain: true })
       .then((authenticatedUser) => {
         let user = utils.queryResultToJson(authenticatedUser)
         const rememberedEmail = insecurity.userEmailFrom(req)
